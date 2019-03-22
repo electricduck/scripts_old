1afc083a49324c2bdb382f32aa6632c3ac464b3f
Ducky <ducky@zyr.io>
2019-03-22 15:49:15 +0000

    Various changes

--- bash/watermark.sh
+++ bash/watermark.sh
@@ -3,12 +3,12 @@
 EXTENSION=".${1##*.}"
 NEW_EXTENSION="_watermarked${EXTENSION}"
 IMAGE_WIDTH=$(identify -ping -format '%w' $1)
-WATERMARK_WIDTH=$(expr $IMAGE_WIDTH / 10)
+WATERMARK_WIDTH=$(expr $IMAGE_WIDTH / $5)
 NEW_FILENAME=${1/$EXTENSION/$NEW_EXTENSION}
 
-convert $2 -resize "$WATERMARK_WIDTH^>" "$2_resized"
+convert $2 -resize "$WATERMARK_WIDTH" "$2_resized"
 composite -gravity $3 -geometry $4 "$2_resized" $1 $NEW_FILENAME
 
-mv $NEW_FILENAME $5
+mv $NEW_FILENAME $6
 
 rm "$2_resized"
